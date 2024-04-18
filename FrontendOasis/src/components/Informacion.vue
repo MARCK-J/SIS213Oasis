163abe41ff017b8a5015bdcec3b95ad609c24759
Luan Huanca <luanhuancam@gmail.com>
2024-04-18 15:53:46 -0400

    Terminacion de pantalla de inicio

---
+++ FrontendOasis/src/components/Informacion.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="agencia-informacion-container">
+    <div class="informacion-content">
+        <h2>Visión</h2>
+        <p>Ser reconocidos como la principal agencia de viajes especializada en
+             destinos latinoamericanos, ofreciendo experiencias únicas y memorables que conecten
+              a nuestros clientes con la riqueza cultural, la belleza natural y la autenticidad de cada país de la región.</p>
+        <h2>Misión</h2>
+        <p>En OASIS, nos comprometemos a proporcionar a nuestros clientes experiencias de viaje excepcionales a través de un servicio personalizado, itinerarios cuidadosamente diseñados y asociaciones con proveedores locales de confianza. Nuestro objetivo es inspirar la exploración, el descubrimiento y el enriquecimiento cultural, creando recuerdos que perduren toda la vida.</p>
+        <h2>Historia</h2>
+        <p>OASIS nació de la pasión por explorar Latinoamérica, fundada por apasionados viajeros con profundos vínculos regionales. Iniciando modestamente en una pequeña oficina, nos hemos convertido en la principal agencia de viajes especializada en destinos latinoamericanos, comprometidos con ofrecer experiencias únicas y auténticas que conecten a nuestros clientes con la riqueza cultural y la belleza natural de la región.</p>
+    </div>
+</div>
+</template>
+<style>
+.agencia-informacion-container{
+    background-color: rgb(177, 188, 188);
+    width: 100%;
+    padding: 50px 0px;
+    display: flex;
+    justify-content: center;
+}
+.informacion-content{
+    gap: 20px;
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
