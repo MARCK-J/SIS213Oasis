c6b4fb3ed48f290fc1e6ff9a2ed8123fb8b6aa36
Luan Huanca <luanhuancam@gmail.com>
2024-05-08 23:59:00 -0400

    Configuracion de gmail con springboot y verificacion de dos pasos

--- FrontendOasis/src/components/Login/TarjetaLogin.vue
+++ FrontendOasis/src/components/Login/TarjetaLogin.vue
@@ -7,21 +7,21 @@
     <form @submit.prevent="continuar" class="form">
       <div class="CustomInput">
         <p>Correo:</p>
-      <input
-        placeholder="Ingrese su correo electronico"
-        type="text"
-        v-model="correo"
-        required
-      />
+        <input
+          placeholder="Ingrese su correo electronico"
+          type="text"
+          v-model="correo"
+          required
+        />
       </div>
       <div class="CustomInput">
         <p>Contraseña:</p>
-      <input
-        placeholder="Ingrese su contraseña"
-        type="password"
-        v-model="password"
-        required
-      />
+        <input
+          placeholder="Ingrese su contraseña"
+          type="password"
+          v-model="password"
+          required
+        />
       </div>
       <router-link to="" class="enlace">¿Olvidaste tu contraseña?</router-link>
       <button type="submit">Continuar</button>
@@ -35,21 +35,20 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { useStore } from 'vuex';
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
 import { decodeCredential } from "vue3-google-login";
 import CustomInput from "./CustomInput.vue";
 import { useRouter } from "vue-router";
-import axios from 'axios';
-
-
+import axios from "axios";
 
 export default defineComponent({
   name: "TarjetaLogin",
   data() {
     return {
-      correo: '',
-      password: ''
+      correo: "",
+      password: "",
+      randomCode: "",
     };
   },
   components: {
@@ -63,87 +62,92 @@ export default defineComponent({
     const callback = (response) => {
       console.log("Inicio de sesión con éxito");
       const user = decodeCredential(response.credential);
-      store.commit('setLoggedIn', true);
-      store.commit('setUser', user);
+      store.commit("setLoggedIn", true);
+      store.commit("setUser", user);
       router.push("/");
     };
     return {
-      callback
+      callback,
     };
   },
 
   methods: {
-    async loginPersona () {
+    async loginPersona() {
       const store = useStore();
       const router = useRouter();
       try {
         // Realiza una solicitud POST al endpoint de inicio de sesión
-        const response = await axios.post('http://localhost:9999/api/v1/cliente/login', {
-          correo: this.correo, // Usa el valor del nombre de usuario del input
-          password: this.password // Usa el valor de la contraseña del input
-        });
+        const response = await axios.post(
+          "http://localhost:9999/api/v1/cliente/login",
+          {
+            correo: this.correo, // Usa el valor del nombre de usuario del input
+            password: this.password, // Usa el valor de la contraseña del input
+          }
+        );
 
         const user = response.data.code;
-        if (user === '200-OK') {
+        if (user === "200-OK") {
           // Si el inicio de sesión es exitoso, guarda el usuario en el store y redirige a la página principal
-          this.$store.commit('setLoggedIn', true);
-          this.$store.commit('setUser', user);
+          this.$store.commit("setLoggedIn", true);
+          this.$store.commit("setUser", user);
+          generateCode();
           //router.push("/");
-          this.$router.push('/Vericacion');
+          this.$router.push("/Vericacion");
         } else {
           // Si el inicio de sesión no es exitoso, muestra un mensaje de error
-          console.error('Error al iniciar sesión:', response.data.message);
-          alert('Error al iniciar sesión: Correo o contraseña incorrectos');
+          console.error("Error al iniciar sesión:", response.data.message);
+          alert("Error al iniciar sesión: Correo o contraseña incorrectos");
         }
-
       } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-        alert('Error al iniciar sesión');
+        console.error("Error al iniciar sesión:", error);
+        alert("Error al iniciar sesión");
       }
     },
 
-    async loginAdmin () {
+    async loginAdmin() {
       const store = useStore();
       const router = useRouter();
       try {
         // Realiza una solicitud POST al endpoint de inicio de sesión
-        const response = await axios.post('http://localhost:9999/api/v1/admin/login', {
-          correo: this.correo, // Usa el valor del nombre de usuario del input
-          password: this.password // Usa el valor de la contraseña del input
-        });
+        const response = await axios.post(
+          "http://localhost:9999/api/v1/admin/login",
+          {
+            correo: this.correo, // Usa el valor del nombre de usuario del input
+            password: this.password, // Usa el valor de la contraseña del input
+          }
+        );
 
         const user = response.data.code;
-        if (user === '200-OK') {
+        if (user === "200-OK") {
           // Si el inicio de sesión es exitoso, guarda el usuario en el store y redirige a la página principal
-          this.$store.commit('setLoggedIn', true);
-          this.$store.commit('setUser', user);
+          this.$store.commit("setLoggedIn", true);
+          this.$store.commit("setUser", user);
           //router.push("/");
-          this.$router.push('/Dashboard');
+          this.$router.push("/Dashboard");
         } else {
           // Si el inicio de sesión no es exitoso, muestra un mensaje de error
-          console.error('Error al iniciar sesión:', response.data.message);
-          alert('Error al iniciar sesión: Correo o contraseña incorrectos');
+          console.error("Error al iniciar sesión:", response.data.message);
+          alert("Error al iniciar sesión: Correo o contraseña incorrectos");
         }
-
       } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-        alert('Error al iniciar sesión');
+        console.error("Error al iniciar sesión:", error);
+        alert("Error al iniciar sesión");
       }
     },
 
     async verificarCorreoAdmin(correo) {
-      if (correo.includes('@oasis')) {
+      if (correo.includes("@oasis")) {
         // El correo contiene el dominio "@oasis", realiza la acción deseada
-        console.log('El correo ingresado pertenece a la empresa');
+        console.log("El correo ingresado pertenece a la empresa");
         await this.loginAdmin();
       } else {
         // El correo no contiene el dominio "@oasis"
-        console.log('El correo ingresado no pertenece a la empresa');
+        console.log("El correo ingresado no pertenece a la empresa");
         await this.loginPersona();
       }
     },
 
-    async continuar (){
+    async continuar() {
       console.log("Usuario:", this.correo);
       console.log("ENTRA", this.correo, this.password);
       // Validar los campos, por ejemplo, si están llenos
@@ -157,26 +161,72 @@ export default defineComponent({
         window.alert("Por favor, llena todos los campos");
       }
     },
-  }
+    generateCode() {
+      randomCode = generateRandomCode();
+      this.$store.commit("setRandomCode", randomCode);
+      sendMail();
+    },
+    sendMail() {
+      const url = "http://localhost:9999/mail/send/" + this.correo;
+      const data = {
+        subject: "Código de Verificación en Dos Pasos para Acceder a tu Cuenta",
+        message: `Estimado/a Usuario/a,
+
+Para completar el proceso de verificación en dos pasos y acceder a tu cuenta de manera segura, por favor utiliza el siguiente código de verificación:
+
+Código de Verificación: ${this.randomCode}
+
+Por favor, asegúrate de introducir este código en el campo correspondiente en la pantalla de inicio de sesión para finalizar el proceso de verificación en dos pasos.
+
+Si no has intentado iniciar sesión o no reconoces esta actividad, por favor contacta con nuestro equipo de soporte de inmediato para tomar las medidas necesarias.
+
+Gracias por tu cooperación en garantizar la seguridad de tu cuenta.
+
+Atentamente,
+
+Agencia de Viajes Oasis
+Max Pasten, Gerente de la agencia de viajes`,
+      };
+
+      // Enviar la solicitud POST usando Axios
+      axios
+        .post(url, data)
+        .then((response) => {
+          console.log("El correo fue enviado exitosamente:", response.data);
+        })
+        .catch((error) => {
+          console.error("Hubo un problema al enviar el correo:", error);
+        });
+    },
+  },
 });
+function generateRandomCode() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
+  for (let i = 0; i < 6; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+}
 </script>
 
 <style>
 .CustomInput {
-    width: 85%;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin: 10px 0px;
+  width: 85%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 10px 0px;
 }
 .CustomInput p {
-    padding: 0;
-    margin: 0;
+  padding: 0;
+  margin: 0;
 }
 .CustomInput input {
-    border: 2px solid black;
-    border-radius: 25px;
-    padding: 10px 20px;
+  border: 2px solid black;
+  border-radius: 25px;
+  padding: 10px 20px;
 }
 .form {
   width: 100%;
--- FrontendOasis/src/components/Verificacion/TarjetaVerificacion.vue
+++ FrontendOasis/src/components/Verificacion/TarjetaVerificacion.vue
@@ -5,12 +5,13 @@
       <h3>Verificacion de dos pasos</h3>
       <p>Se le envio un codigo de verificacion a su correo</p>
       <p>por favor ingrese ese codigo en el siguiente campo para la validacion</p>
-      <form @submit.prevent="" class="form">
+      <form @submit.prevent="verificarCodigo" class="form">
         <div class="CustomInput">
           <p>Codigo de verificacion:</p>
         <input
           placeholder="Ingrese su codigo de verificacion"
           type="text"
+          v-model="verificacion"
           required
         />
         </div>
@@ -18,6 +19,40 @@
       </form>
     </div>
   </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "VerificacionDosPasos",
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const verificacion = ''; // Variable para almacenar el código de verificación ingresado por el usuario
+
+    const verificarCodigo = () => {
+      // Obtener el código de verificación almacenado en Vuex
+      const codigoGuardado = store.state.codigoVerificacion;
+      
+      // Verificar si el código ingresado coincide con el código guardado
+      if (verificacion === codigoGuardado) {
+        // Código correcto, realizar la acción deseada (por ejemplo, redireccionar a otra página)
+        router.push('/');
+      } else {
+        // Código incorrecto, mostrar un mensaje de error o realizar alguna otra acción
+        alert('El código de verificación ingresado es incorrecto. Por favor, inténtelo de nuevo.');
+      }
+    };
+
+    return {
+      verificacion,
+      verificarCodigo
+    };
+  }
+});
+</script>
+
   <style>
   .CustomInput {
       width: 85%;
