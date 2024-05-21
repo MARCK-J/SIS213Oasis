cfc3b983ec428f6980c0c52746bceb6c2e68956a
Luis Huanca <luanhuancam@gmail.com>
2024-05-20 21:42:57 -0400

    pantalla admin inicio

---
+++ FrontendOasis/src/components/Admin/Admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="inspire">
+    <Sidebar :drawer="drawer" @update:drawer="drawer = $event" />
+    <Topbar @update:drawer="drawer = $event" />
+    <v-main style="background: #f5f5f540">
+      <v-container class="py-8 px-6" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from "./Sidebar.vue";
+import Topbar from "./Topbar.vue";
+
+export default {
+  name: "App",
+  components: { Topbar, Sidebar },
+  data() {
+    return {
+      cards: ["Today", "Yesterday"],
+      drawer: null,
+    };
+  },
+  methods: {},
+};
+</script>
