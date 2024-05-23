23b34e1136b6d8ee086c50501d3594c2af1a8f47
Luis Huanca <luanhuancam@gmail.com>
2024-05-22 22:32:36 -0400

    Avances en sidebar del dashboard de admin

--- FrontendOasis/src/views/Dashboard.vue
+++ FrontendOasis/src/views/Dashboard.vue
@@ -1,25 +1,65 @@
 <template>
-  <div>
-    <nav-bar/>
-    <TableAudit/>
-  </div>
+  <nav-bar/>
+    <div class="DashboardContainer">
+      <SideBar class="barralateral" @optionSelected="optionSelected"/>
+      <div class="content">
+        <component :is="currentComponent" />
+      </div>
+    </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import SideBar from "../components/Admin/SideBar.vue";
 import TableAudit from "../components/Dashboard/TableAudit.vue";
 
+
 export default {
-  name: "home-view",
   components: {
     NavBar,
-    TableAudit
-
+    SideBar,
+    TableAudit,
   },
-};
+  data() {
+    return {
+      currentComponent: 'TableAudit'
+    }
+  },
+  methods: {
+    optionSelected(option) {
+      switch (option) {
+        case 'admin':
+          this.currentComponent = 'TableAudit';
+          break;
+        case 'flights':
+          this.currentComponent = 'Flights';
+          break;
+        case 'hotels':
+          this.currentComponent = 'Hotels';
+          break;
+        case 'cars':
+          this.currentComponent = 'Cars';
+          break;
+        default:
+          this.currentComponent = 'TableAudit';
+      }
+    }
+  }
+}
 </script>
 
-
 <style scoped>
+
+.DashboardContainer {
+  display: flex;
+  flex-direction: row;
+}
+
+.barralateral{
+  width: 15%;
+}
+.content{
+  width: 85%;
+}
 
 </style>
