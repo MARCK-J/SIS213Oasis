3c1f58300d0766fb68eeceee812e20c42990c867
Luis Huanca <luanhuancam@gmail.com>
2024-06-07 22:30:22 -0400

    arreglo de envio de mensajes gmail, con firma digital

--- BackendOasis/src/main/java/com/ucb/SIS213/Oasis/email/Service/MailService.java
+++ BackendOasis/src/main/java/com/ucb/SIS213/Oasis/email/Service/MailService.java
@@ -2,27 +2,40 @@ package com.ucb.SIS213.Oasis.email.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.ucb.SIS213.Oasis.email.Model.MailStructure;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
 @Service
 public class MailService {
-    
+
     @Autowired
     private JavaMailSender mailSender;
+
     @Value("${spring.mail.username}")
     private String fromMail;
 
-    public void sendMail(String mail, MailStructure mailStructure){
-        SimpleMailMessage mensaje = new SimpleMailMessage();
-        mensaje.setFrom(fromMail);
-        mensaje.setSubject(mailStructure.getSubject());
-        mensaje.setText(mailStructure.getMessage());
-        mensaje.setTo(mail);
+    public void sendMail(String mail, MailStructure mailStructure) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
 
-        mailSender.send(mensaje);    }
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromMail);
+        helper.setTo(mail);
+        helper.setSubject(mailStructure.getSubject());
 
+        String textWithSignature = mailStructure.getMessage() + "<br><br>Firma:<br><img src='cid:signatureImage'>";
+
+        helper.setText(textWithSignature, true); 
+
+        ClassPathResource signatureImage = new ClassPathResource("static/FirmaDigitalMax.png");
+        helper.addInline("signatureImage", signatureImage);
+
+        mailSender.send(message);
+    }
 }
